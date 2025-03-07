dde1d43f30e362a43c525b588ef015a993666908
Leonor Cardoso <leonorjcdc@gmail.com>
2025-03-07 19:51:28 +0000

    HomePage full screen

--- tech_away/src/pages/HomePage.jsx
+++ tech_away/src/pages/HomePage.jsx
@@ -19,7 +19,12 @@ export default function HomePage() {
 	}, []);
 
 	return (
-		<div style={{ backgroundColor: "var(--light-grey)" }}>
+		<div
+			style={{
+				backgroundColor: "var(--light-grey)",
+				color: "var(--dark-grey)",
+			}}
+		>
 			{isMobile ? <SmNavbar /> : <InitialNavBar />}
 			<Container className="vh-100">
 				<Stack
@@ -32,7 +37,7 @@ export default function HomePage() {
 						WebkitMaskImage: "inset(0 round var(--rounded-lg))",
 						maskImage: "inset(0 round var(--rounded-lg))",
 					}}
-					className="mt-5 justify-content-between overflow-hidden"
+					className="mt-4 justify-content-between overflow-hidden"
 				>
 					<svg
 						width="253"
@@ -50,9 +55,6 @@ export default function HomePage() {
 					</svg>
 					<h1
 						style={{
-							color: "var(--dark-grey)",
-							fontFamily: "var(--title-font)",
-
 							fontSize: "64px",
 						}}
 					>
@@ -89,7 +91,10 @@ export default function HomePage() {
 					</svg>
 				</Stack>
 
-				<Stack direction="horizontal">
+				<Stack
+					direction="horizontal"
+					style={{ fontFamily: "var(--body-font)" }}
+				>
 					<div
 						style={{
 							backgroundColor: "var(--white)",
@@ -99,40 +104,238 @@ export default function HomePage() {
 							height: "539px",
 							WebkitMaskImage: "inset(0 round var(--rounded-lg))",
 							maskImage: "inset(0 round var(--rounded-lg))",
+							position: "relative",
+							overflow: "hidden",
 						}}
-						className="mt-3 overflow-hidden"
+						className="mt-4 "
 					>
-						{/* <svg
-							width="559"
-							height="554"
-							viewBox="0 0 559 554"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
+						{/* Background */}
+						<div
+							style={{ position: "absolute", width: "100%", height: "100%" }}
 						>
-							<path
-								d="M31.0907 11.8915C16.2225 71.7413 0.113726 131.302 22.8298 187.875C46.2484 246.198 95.6589 280.411 163.087 261.617C204.366 250.111 240.888 217.379 253.421 176.213C258.834 158.431 259.246 140.37 247.635 127.094C237.331 115.31 219.239 135.035 212.026 143.591C176.874 185.291 164.459 234.932 160.572 285.598C155.612 350.268 168.376 410.887 223.789 445.93C300.758 494.606 448.962 501.693 507.136 400.946C522.825 373.775 546.956 333.667 536.026 303.199C529.022 283.676 511.582 274.846 489.63 281.907C458.842 291.809 433.644 327.527 421.169 354.955C404.584 391.418 400.882 437.318 422.167 468.662C438.731 493.055 514.063 544.436 546.614 542.427"
-								stroke="#B5A8C9"
-								stroke-width="23"
-								stroke-linecap="round"
-							/>
-						</svg>
+							{/* SVG no fundo */}
+							<svg
+								width="559"
+								height="554"
+								viewBox="0 0 559 554"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								style={{
+									position: "absolute",
+									top: "-10",
+									left: "10",
+									width: "100%",
+									height: "100%",
+									zIndex: "1", // Está mais atrás
+								}}
+							>
+								<path
+									d="M31.0907 11.8915C16.2225 71.7413 0.113726 131.302 22.8298 187.875C46.2484 246.198 95.6589 280.411 163.087 261.617C204.366 250.111 240.888 217.379 253.421 176.213C258.834 158.431 259.246 140.37 247.635 127.094C237.331 115.31 219.239 135.035 212.026 143.591C176.874 185.291 164.459 234.932 160.572 285.598C155.612 350.268 168.376 410.887 223.789 445.93C300.758 494.606 448.962 501.693 507.136 400.946C522.825 373.775 546.956 333.667 536.026 303.199C529.022 283.676 511.582 274.846 489.63 281.907C458.842 291.809 433.644 327.527 421.169 354.955C404.584 391.418 400.882 437.318 422.167 468.662C438.731 493.055 514.063 544.436 546.614 542.427"
+									stroke="#B5A8C9"
+									strokeWidth="23"
+									strokeLinecap="round"
+								/>
+							</svg>
 
-						<Image src="../../public/assets/telemovel.png" /> */}
-						<Stack>
+							{/* Imagem sobreposta ao SVG */}
+							<Image
+								src="../../public/assets/telemovel.png"
+								style={{
+									position: "absolute",
+									top: "75%",
+									left: "50%",
+									transform: "translate(-50%, -50%)",
+									maxWidth: "80%",
+									zIndex: "2", // Sobre o SVG
+								}}
+							/>
+						</div>
+
+						{/* Links*/}
+						<Stack
+							direction="horizontal"
+							gap={3}
+							style={{
+								position: "relative",
+								zIndex: "3",
+								width: "564px",
+								height: "539px",
+							}}
+							className="px-5 py-4 justify-content-end align-items-end"
+						>
 							<Link
 								screen="store"
+								style={{
+									backgroundColor: "var(--variant-two)",
+									boxShadow: "var(--shadow-default)",
+									textDecoration: "none",
+									color: "var(--dark-grey)",
+									height: "50px",
+								}}
+								className="px-4 rounded-pill d-flex justify-content-center align-items-center"
+							>
+								Check our store
+							</Link>
+							<Link
 								style={{
 									backgroundColor: "var(--variant-one)",
 									boxShadow: "var(--shadow-default)",
 									textDecoration: "none",
 									color: "var(--dark-grey)",
+									width: "50px",
+									height: "50px",
 								}}
-								className="rounded-pill"
+								className="rounded-circle d-flex justify-content-center align-items-center"
 							>
-								Check our store
+								{/* seta */}
+								<svg
+									width="20"
+									height="20"
+									viewBox="0 0 20 20"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M1.40029 18.4003L18.373 1.42763M18.373 1.42763L6.40251 2.5973M18.373 1.42763L17.2033 13.3981"
+										stroke="#27272D"
+										strokeWidth="2"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+									/>
+								</svg>
 							</Link>
 						</Stack>
 					</div>
+
+					<Stack className="mt-4 ms-4" gap={4}>
+						<Stack
+							style={{
+								backgroundColor: "var(--variant-one)",
+								boxShadow: "var(--shadow-default)",
+								borderRadius: "var(--rounded-lg)",
+							}}
+							className="justify-content-center align-items-center"
+						>
+							<p style={{ fontSize: "32px" }}>
+								100k + people joined the plataform
+							</p>
+							<Link style={{ textDecoration: "none" }}>
+								<Stack
+									direction="horizontal"
+									gap={2}
+									style={{
+										backgroundColor: "var(--dark-grey)",
+										color: "var(--dark-grey)",
+										fontSize: "20px",
+									}}
+									className="px-4 py-1 rounded-pill justify-content-center align-items-center"
+								>
+									<p
+										className="m-0"
+										style={{
+											color: "var(--light-grey",
+										}}
+									>
+										Join
+									</p>
+									<svg
+										width="18"
+										height="12"
+										viewBox="0 0 18 12"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M1 6H16.5M16.5 6L10.5 1M16.5 6L10.5 11"
+											stroke="#E7E7E7"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										/>
+									</svg>
+								</Stack>
+							</Link>
+						</Stack>
+
+						<Stack
+							style={{
+								backgroundColor: "var(--white)",
+								boxShadow: "var(--shadow-default)",
+								borderRadius: "var(--rounded-lg)",
+							}}
+							className="justify-content-center align-items-center"
+							gap={4}
+							direction="vertical"
+						>
+							<h3
+								style={{
+									fontSize: "32px",
+									fontWeight: "bold",
+								}}
+								className="m-0 mt-5"
+							>
+								Our Services
+							</h3>
+							<Stack gap={3}>
+								<Stack
+									direction="horizontal"
+									gap={3}
+									className="justify-content-center"
+								>
+									<Link
+										style={{
+											textDecoration: "none",
+											color: "var(--dark-grey)",
+											fontSize: "20px",
+											backgroundColor: "var(--variant-two)",
+										}}
+										className="rounded-pill px-4 py-2"
+									>
+										Sell and Buy Equipment
+									</Link>
+									<Link
+										style={{
+											textDecoration: "none",
+											color: "var(--dark-grey)",
+											fontSize: "20px",
+											backgroundColor: "var(--variant-one)",
+										}}
+										className="rounded-pill px-4 py-2"
+									>
+										Request Equipment Repair
+									</Link>
+								</Stack>
+								<Stack
+									direction="horizontal"
+									gap={3}
+									className="justify-content-center"
+								>
+									<Link
+										style={{
+											textDecoration: "none",
+											color: "var(--dark-grey)",
+											fontSize: "20px",
+											backgroundColor: "var(--variant-one)",
+										}}
+										className="rounded-pill px-4 py-2"
+									>
+										Create Beneficiary Project
+									</Link>
+									<Link
+										style={{
+											textDecoration: "none",
+											color: "var(--dark-grey)",
+											fontSize: "20px",
+											backgroundColor: "var(--variant-two)",
+										}}
+										className="rounded-pill px-4 py-2"
+									>
+										Donate Equipment
+									</Link>
+								</Stack>
+							</Stack>
+						</Stack>
+					</Stack>
 				</Stack>
 			</Container>
 		</div>
