30d48906c9f99089915fcc19e1e6141d6c19def3
Gustavo Machado <gustavomacmac@gmail.com>
2025-03-09 18:28:05 +0000

    init registerForms for employee
    create employeeRoles get and post routes. React Router Layout test

--- server/routes/employeeRole.js
+++ server/routes/employeeRole.js
@@ -2,13 +2,31 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models')
 
-router.get("/", (req, res) => {
-
-});
-
-router.post("/", (req, res) => {
-
-});
+router.get("/", async (req, res) => {
+    try {
+      const employeeRoles = await models.EmployeeRole.findAll();
+      res.json(employeeRoles);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Erroe fetching roles." });
+    }
+  });
+  
+  // Rota para criar um novo EmployeeRole
+  router.post("/", async (req, res) => {
+    try {
+      const newEmployeeRole = await models.EmployeeRole.create({
+        role:  req.body.role,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      });
+      
+      res.status(201).json(newEmployeeRole);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error creating roles." });
+    }
+  });
 
 router.get("/:ID", (req, res) => {
 
