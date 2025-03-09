ea310ebcbb392f10709aa29463ab3a2517b7a7ba
Leonor Cardoso <leonorjcdc@gmail.com>
2025-03-09 23:17:12 +0000

    delete

--- server/routes/client.js
+++ server/routes/client.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 const models = require("../models");
 
+router.get("/", async (req, res) => {
+	try {
+		const client = await models.Client.findAll();
+		if (!client) {
+			return res.status(404).json({ error: "Clients not found" });
+		}
+		res.json(client);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
 router.get("/:NIC", async (req, res) => {
 	try {
-		const client = await Client.findByPk(req.params.NIC);
+		const client = await models.Client.findByPk(req.params.NIC);
 		if (!client) {
 			return res.status(404).json({ error: "Client not found" });
 		}
@@ -16,7 +28,7 @@ router.get("/:NIC", async (req, res) => {
 
 router.put("/:NIC", async (req, res) => {
 	try {
-		const client = await Client.findByPk(req.params.NIC);
+		const client = await models.Client.findByPk(req.params.NIC);
 		if (!client) {
 			return res.status(404).json({ error: "Client not found" });
 		}
@@ -29,7 +41,7 @@ router.put("/:NIC", async (req, res) => {
 
 router.delete("/:NIC", async (req, res) => {
 	try {
-		const client = await Client.findByPk(req.params.NIC);
+		const client = await models.Client.findByPk(req.params.NIC);
 		if (!client) {
 			return res.status(404).json({ error: "Client not found" });
 		}
@@ -42,7 +54,7 @@ router.delete("/:NIC", async (req, res) => {
 
 router.post("/", async (req, res) => {
 	try {
-		const client = await Client.create(req.body);
+		const client = await models.Client.create(req.body);
 		res.status(201).json(client);
 	} catch (error) {
 		res.status(400).json({ error: error.message });
