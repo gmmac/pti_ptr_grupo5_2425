cd71dd8910fa28cd277273ed9aa2f4ee8717dc04
fc58817 <fc58817@alunos.fc.ul.pt>
2025-03-13 22:41:52 +0000

    starting repair

--- server/routes/repairStatus.js
+++ server/routes/repairStatus.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const models = require('../models')
+const models = require("../models");
 
-router.get("/", (req, res) => {
-
+router.get("/", async (req, res) => {
+  try {
+    const repairStatus = await models.RepairStatus.findAll();
+    res.json({
+      data: repairStatus,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching repair status." });
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.get("/:ID", (req, res) => {});
 
-router.get("/:ID", (req, res) => {
+router.put("/:ID", (req, res) => {});
 
-});
-
-router.put("/:ID", (req, res) => {
-
-});
-
-router.delete("/:ID", (req, res) => {
-    
-});
+router.delete("/:ID", (req, res) => {});
 
 module.exports = router;
