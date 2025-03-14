309cd17e21e92744128cdb8b7e093d1d374582db
fc58817 <fc58817@alunos.fc.ul.pt>
2025-03-14 00:10:21 +0000

    repair fixed

--- server/models/repair.js
+++ server/models/repair.js
@@ -1,56 +1,67 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Repair extends Model {
     static associate(models) {
       Repair.belongsTo(models.RepairStatus, {
-        foreignKey: 'statusID',
-        targetKey: 'id',
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL'
+        foreignKey: "statusID",
+        targetKey: "id",
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
       });
 
       Repair.belongsTo(models.Employee, {
-        foreignKey: 'employeeId',
-        targetKey: 'nic',
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL'
+        foreignKey: "employeeId",
+        targetKey: "nic",
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      });
+      Repair.belongsTo(models.UsedEquipment, {
+        foreignKey: "usedEquipmentId",
+        targetKey: "id",
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
       });
     }
   }
-  Repair.init({
-    repairNumber: {
-      type: DataTypes.STRING(6),
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: false
+  Repair.init(
+    {
+      repairNumber: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      statusID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING(500),
+        allowNull: true,
+      },
+      budget: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      estimatedDeliverDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      employeeId: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      usedEquipmentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    statusID: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.STRING(500),
-      allowNull: true
-    },
-    budget: {
-      type: DataTypes.FLOAT,
-      allowNull: true
-    },
-    completionDate: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    employeeId: {
-      type: DataTypes.STRING(20),
-      allowNull: false
+    {
+      sequelize,
+      modelName: "Repair",
+      timestamps: true,
     }
-  }, {
-    sequelize,
-    modelName: 'Repair',
-    timestamps: true,
-  });
+  );
   return Repair;
 };
