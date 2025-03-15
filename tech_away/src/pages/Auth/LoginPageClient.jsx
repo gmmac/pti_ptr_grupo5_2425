f8341d3c0b1f0c63693bac03a2bf6ae7c37dc13b
NosisMastah <diogorq2@gmail.com>
2025-03-15 19:10:56 +0000

    Resolvido problema na margem da pagina de login para lidar com a presenca da navbar

--- tech_away/src/pages/Auth/LoginPageClient.jsx
+++ tech_away/src/pages/Auth/LoginPageClient.jsx
@@ -1,12 +1,11 @@
+// LoginPageClient.js
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Stack } from "react-bootstrap";
 import LoginForms from '../../components/authentication/LoginForms';
 
-const mobileSize = 768;
-
 export default function LoginPageClient() {
     return (
-        <div className="d-flex align-items-center justify-content-center overflow-hidden">
+        <div className="d-flex align-items-center justify-content-center" style={{ minHeight: "calc(100vh - 56px)" }}>
             <div style={{ width: "700px", maxWidth: "100%", padding: "20px" }}>
                 <LoginForms />
             </div>
