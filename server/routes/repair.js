f93319898df170187b1d4339148561b3dc4eb1cf
João Sequinho <joao.sequinho@hotmail.com>
2025-03-18 15:14:08 +0000

    repair route updates

--- server/routes/repair.js
+++ server/routes/repair.js
@@ -1,26 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const models = require('../models')
+const models = require("../models");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const repair = await models.Repair.findAll();
+    res.status(200).json({
+      data: repair,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching repairs." });
+  }
+});
+//falta a paginação
 
+router.post("/", async (req, res) => {
+  try {
+    const {
+      statusID,
+      description,
+      budget,
+      estimatedDeliverDate,
+      employeeId,
+      usedEquipmentId,
+    } = req.body;
+    const repair = await models.Repair.create({
+      statusID,
+      description,
+      budget,
+      estimatedDeliverDate,
+      employeeId,
+      usedEquipmentId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    res.status(200).json({
+      data: repair,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error creating repair." });
+  }
 });
 
-router.post("/", (req, res) => {
-
+router.get("/:id", async (req, res) => {
+  try {
+    const repair = await models.Repair.findByPk(req.params.id);
+    if (!repair) return res.status(404).json({ error: "Repair not found." });
+    res.status(200).json(repair);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching repair." });
+  }
 });
 
-router.get("/:ID", (req, res) => {
+router.put("/:id", async (req, res) => {});
 
-});
-
-router.put("/:ID", (req, res) => {
-
-});
-
-router.delete("/:ID", (req, res) => {
-    
-});
-
+router.delete("/:id", async (req, res) => {});
 
 module.exports = router;
---
+++ tech_away/src/pages/RepairsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RepairsPage() {
+  return (
+    <div>RepairsPage</div>
+  )
+}
