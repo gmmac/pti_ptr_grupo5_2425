7632d0fdcc87e3febd24053f7251fe5fbaa228e6
Leonor Cardoso <leonorjcdc@gmail.com>
2025-03-27 18:07:37 +0000

    feat: enhance FiltersBarEquipmentSheet with search functionality and clear button; update TestPage to pass filter parameters

--- tech_away/src/pages/TestPage.jsx
+++ tech_away/src/pages/TestPage.jsx
@@ -5,28 +5,28 @@ import DisplayTable from "../components/equipment/DisplayTable";
 import FiltersBarEquipmentSheet from "../components/equipment/FiltersBarEquipmentSheet";
 
 export default function TestPage() {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-  const [paramsTofilter, setParamsTofilter] = useState({});
+	const [paramsTofilter, setParamsTofilter] = useState({});
 
-  const handleFilterSet = (params) => {
-    setParamsTofilter(params);
-  };
-  return (
-    <Container>
-      <Stack gap={3} className="justify-content-center align-items-center">
-        <h2>Test Page</h2>
-        <Button variant="primary" onClick={handleShow}>
-          Criar EquipmentSheet
-        </Button>
-        <FiltersBarEquipmentSheet paramsTofilter={handleFilterSet} />
-        <DisplayTable model="equipmentSheet" params="" />
-      </Stack>
+	const handleFilterSet = (params) => {
+		setParamsTofilter(params);
+	};
+	return (
+		<Container style={{ backgroundColor: "var(--light-grey)" }}>
+			<Stack gap={3} className="justify-content-center align-items-center">
+				<h2>Test Page</h2>
+				<Button variant="primary" onClick={handleShow}>
+					Criar EquipmentSheet
+				</Button>
+				<FiltersBarEquipmentSheet paramsTofilter={handleFilterSet} />
+				<DisplayTable model="equipmentSheet" params={paramsTofilter} />
+			</Stack>
 
-      <FormsEquipmentSheet showModal={show} closeModal={handleClose} />
-    </Container>
-  );
+			<FormsEquipmentSheet showModal={show} closeModal={handleClose} />
+		</Container>
+	);
 }
