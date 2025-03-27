21cb9a67c39a6ca806b6ab6f31e910af225ff7c2
fc60280 <leonorjcdc@gmail.com>
2025-03-27 11:22:42 +0000

    feat: add FiltersBarEquipmentSheet component for enhanced filtering capabilities; integrate with TestPage for dynamic parameter handling

--- tech_away/src/components/equipment/FiltersBarEquipmentSheet.jsx
+++ tech_away/src/components/equipment/FiltersBarEquipmentSheet.jsx
@@ -1,11 +1,56 @@
 import React from "react";
-import { Stack } from "react-bootstrap";
+import { Button, Form, Stack } from "react-bootstrap";
 
-export default function FiltersBarEquipmentSheet( ) {
-	const paramsTofilter = [
-		{ name: "barcode", type: "text" },
-		{ name: "model", type: "text" },
-		{ name: "type", type: "text" },
-	];
-	return <Stack direction="horizontal"> </Stack>;
+export default function FiltersBarEquipmentSheet({ paramsTofilter }) {
+  const params = [
+    { name: "barcode", value: "" },
+    { name: "model", value: "" },
+    { name: "type", value: "" },
+  ];
+
+  const handleFilter = (e) => {
+    const { name, value } = e.target;
+    paramsTofilter((prev) => ({ ...prev, [name]: value }));
+  };
+  return (
+    <Stack
+      direction="horizontal"
+      style={{
+        backgroundColor: "var(--white)",
+        boxShadow: "var(--shadow-default)",
+      }}
+      gap={3}
+      className="p-2 justify-content-center align-items-center rounded-pill"
+    >
+      <Form.Control
+        type="text"
+        placeholder="Search by "
+        name="search"
+        onChange={handleFilter}
+        className="rounded-pill w-50"
+        style={{ backgroundColor: "var(--white)" }}
+      />
+      <Form.Select
+        name="Search by:"
+        className="rounded-pill w-25"
+        style={{
+          backgroundColor: "var(--variant-one)",
+          color: "var(--dark-grey)",
+        }}
+      >
+        <option value="">Filtrar por:</option>
+        {params.map((param) => (
+          <option key={param.name} value={param.name}>
+            {param.name}
+          </option>
+        ))}
+      </Form.Select>
+      <Button
+        className="rounded-pill"
+        style={{ backgroundColor: "var(--variant-two)", border: "none" }}
+      >
+        Search
+      </Button>
+    </Stack>
+  );
 }
--- tech_away/src/pages/TestPage.jsx
+++ tech_away/src/pages/TestPage.jsx
@@ -2,25 +2,31 @@ import React, { useEffect, useState } from "react";
 import { Button, Container, Stack } from "react-bootstrap";
 import FormsEquipmentSheet from "../components/equipment/FormsEquipmentSheet";
 import DisplayTable from "../components/equipment/DisplayTable";
+import FiltersBarEquipmentSheet from "../components/equipment/FiltersBarEquipmentSheet";
 
 export default function TestPage() {
-	const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-	return (
-		<Container>
-			<Stack gap={3} className="justify-content-center align-items-center">
-				<h2>Test Page</h2>
-				<Button variant="primary" onClick={handleShow}>
-					Criar EquipmentSheet
-				</Button>
+  const [paramsTofilter, setParamsTofilter] = useState({});
 
-				<DisplayTable model="equipmentSheet" params="" />
-			</Stack>
+  const handleFilterSet = (params) => {
+    setParamsTofilter(params);
+  };
+  return (
+    <Container>
+      <Stack gap={3} className="justify-content-center align-items-center">
+        <h2>Test Page</h2>
+        <Button variant="primary" onClick={handleShow}>
+          Criar EquipmentSheet
+        </Button>
+        <FiltersBarEquipmentSheet paramsTofilter={handleFilterSet} />
+        <DisplayTable model="equipmentSheet" params="" />
+      </Stack>
 
-			<FormsEquipmentSheet showModal={show} closeModal={handleClose} />
-		</Container>
-	);
+      <FormsEquipmentSheet showModal={show} closeModal={handleClose} />
+    </Container>
+  );
 }
