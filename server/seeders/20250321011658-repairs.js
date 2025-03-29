709f2acad48354177e449c5662b6a975dd4c5ad4
NosisMastah <diogorq2@gmail.com>
2025-03-29 22:05:52 +0000

    criados inserts para os seeders da reparação, cliente e employee

--- server/seeders/20250321011658-repairs.js
+++ server/seeders/20250321011658-repairs.js
@@ -3,7 +3,30 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-
+    await queryInterface.bulkInsert("Repairs", [
+      { 
+        statusID: 1, 
+        description: "A playstation está a fazer muito barulho", 
+        budget: 150,
+        estimatedDeliverDate: new Date(),
+        employeeId: 123456789,
+        clientId: 123456789,
+        usedEquipmentId: 5,
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      },
+      { 
+        statusID: 1, 
+        description: "O telemóvel deixou de ligar", 
+        budget: 150,
+        estimatedDeliverDate: new Date(),
+        employeeId: 123456789,
+        clientId: 123456789,
+        usedEquipmentId: 1,
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      },
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
