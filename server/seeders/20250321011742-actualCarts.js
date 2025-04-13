1cac5bd6a683442512d3f3fe0ff85e717069a382
NosisMastah <diogorq2@gmail.com>
2025-04-13 19:32:42 +0100

    Update no seeder para remover cliente inexistente

--- server/seeders/20250321011742-actualCarts.js
+++ server/seeders/20250321011742-actualCarts.js
@@ -2,26 +2,56 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		await queryInterface.bulkInsert(
-			"ActualCarts",
-			[
-				{
-					clientNIC: "123456789",
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				},
-				{
-					clientNIC: "987654321",
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				},
-			],
-			{}
-		);
-	},
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert(
+            "ActualCarts",
+            [
+                {
+                    clientNIC: "123456789",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "123456788",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "895235746",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "987654321",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "987654323",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "987654324",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "987654325",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    clientNIC: "987654326",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
 
-	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete("ActualCarts", null, {});
-	},
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete("ActualCarts", null, {});
+    },
 };
