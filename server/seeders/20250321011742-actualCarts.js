f9aee5547b55a844905f2302108889dcb816777c
Leonor Cardoso <leonorjcdc@gmail.com>
2025-04-13 17:51:08 +0100

    Add additional clientNIC entries to ActualCarts seeder

--- server/seeders/20250321011742-actualCarts.js
+++ server/seeders/20250321011742-actualCarts.js
@@ -12,7 +12,42 @@ module.exports = {
 					updatedAt: new Date(),
 				},
 				{
+					clientNIC: "123456788",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					clientNIC: "895235746",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					clientNIC: "895235711",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
 					clientNIC: "987654321",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					clientNIC: "987654323",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					clientNIC: "987654324",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					clientNIC: "987654325",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					clientNIC: "987654326",
 					createdAt: new Date(),
 					updatedAt: new Date(),
 				},
