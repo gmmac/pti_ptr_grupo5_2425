98ea840d3513c06f895564d649684fd0d2976372
Gustavo Machado <gustavomacmac@gmail.com>
2025-04-30 16:49:40 +0100

    ips update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: ["http://localhost:5173", "http://34.51.129.238", "https://techaway.dev/"],
 		credentials: true,
 	})
 );
