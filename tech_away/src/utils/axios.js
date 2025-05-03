e8756b5eaee95994943f77abf7f5a773de2a221b
Gustavo Machado <gustavomacmac@gmail.com>
2025-05-03 12:15:03 +0100

    deployment port

--- tech_away/src/utils/axios.js
+++ tech_away/src/utils/axios.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+//localhost
 axios.defaults.baseURL =
   window.location.protocol + '//' +
   window.location.hostname + ':' + 4005;
+
+  //deploy
+axios.defaults.baseURL =
+  window.location.protocol + '//' +
+  window.location.hostname;
 
 axios.defaults.withCredentials = true;
 
