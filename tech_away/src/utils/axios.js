9a5280ee9584915d9c6bdba7b7262aacbac7aec8
Gustavo Machado <fc60249@alunos.fc.ul.pt>
2025-05-08 12:55:36 +0100

    Update axios.js

--- tech_away/src/utils/axios.js
+++ tech_away/src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 //localhost
-axios.defaults.baseURL =
-  window.location.protocol + '//' +
-  window.location.hostname + ':' + 4005;
+// axios.defaults.baseURL =
+//   window.location.protocol + '//' +
+//   window.location.hostname + ':' + 4005;
 
   //deploy
 axios.defaults.baseURL =
