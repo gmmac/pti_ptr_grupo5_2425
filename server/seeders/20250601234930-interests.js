8ab30a86d72791c0d6631c6e587883f90e16417f
Leonor Cardoso <leonorjcdc@gmail.com>
2025-06-19 16:05:32 +0100

    feat: add seed data for Interests, FolderInterests, and PreferredStoresInterets

--- server/seeders/20250601234930-interests.js
+++ server/seeders/20250601234930-interests.js
@@ -1,25 +1,77 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert("Interests", [
+			{
+				clientNic: "895235746",
+				brandID: 1,
+				modelID: 12,
+				typeID: 6,
+				equipmentSheetID: "10000000000000000012",
+				equipmentStatusID: 3,
+				minLaunchYear: 2023,
+				maxLaunchYear: 2025,
+				minPrice: 50.0,
+				maxPrice: 100.0,
+				description: null,
+				notified: false,
+				createdAt: new Date("2025-06-19T14:25:11.975Z"),
+				updatedAt: new Date("2025-06-19T14:25:11.975Z"),
+			},
+			{
+				clientNic: "895235746",
+				brandID: 1,
+				modelID: null,
+				typeID: 1,
+				equipmentSheetID: null,
+				equipmentStatusID: 2,
+				minLaunchYear: 2022,
+				maxLaunchYear: 2025,
+				minPrice: 575.0,
+				maxPrice: 850.0,
+				description: "Um iphone quase novo e com a bateria em bom estado",
+				notified: false,
+				createdAt: new Date("2025-06-19T14:41:18.536Z"),
+				updatedAt: new Date("2025-06-19T14:41:18.536Z"),
+			},
+			{
+				clientNic: "895235746",
+				brandID: null,
+				modelID: null,
+				typeID: 8,
+				equipmentSheetID: null,
+				equipmentStatusID: 3,
+				minLaunchYear: 2020,
+				maxLaunchYear: 2023,
+				minPrice: 175.0,
+				maxPrice: 375.0,
+				description: "Sem preferência na marca",
+				notified: false,
+				createdAt: new Date("2025-06-19T14:43:17.185Z"),
+				updatedAt: new Date("2025-06-19T14:43:17.185Z"),
+			},
+			{
+				clientNic: "895235746",
+				brandID: 2,
+				modelID: 2,
+				typeID: 1,
+				equipmentSheetID: "23456789012345678901",
+				equipmentStatusID: 2,
+				minLaunchYear: null,
+				maxLaunchYear: null,
+				minPrice: null,
+				maxPrice: null,
+				description: null,
+				notified: false,
+				createdAt: new Date("2025-06-19T14:43:47.179Z"),
+				updatedAt: new Date("2025-06-19T14:43:47.179Z"),
+			},
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete();
+	},
 };
--- server/seeders/20250601234949-folderInterests.js
+++ server/seeders/20250601234949-folderInterests.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert("FolderInterests", [
+			{
+				name: "Favoritos",
+				clientNIC: "895235746",
+				createdAt: new Date("2025-06-19T15:01:14.943Z"),
+				updatedAt: new Date("2025-06-19T15:01:14.943Z"),
+			},
+			{
+				name: "Smartphones",
+				clientNIC: "895235746",
+				createdAt: new Date("2025-06-19T15:04:02.758Z"),
+				updatedAt: new Date("2025-06-19T15:04:02.758Z"),
+			},
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete();
+	},
 };
