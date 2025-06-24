5d9b449f51968438beee393f029b3a7c171b011d
Leonor Cardoso <leonorjcdc@gmail.com>
2025-06-24 18:51:48 +0100

    interest seeders

--- server/seeders/20250601234949-folderInterests.js
+++ server/seeders/20250601234949-folderInterests.js
@@ -2,10 +2,26 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-  },
-  
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert("FolderInterests", [
+			{
+				id: 1,
+				name: "Apple Products",
+				clientNIC: "895235746",
+				createdAt: new Date("2025-06-24T17:44:27.603Z"),
+				updatedAt: new Date("2025-06-24T17:44:27.603Z"),
+			},
+			{
+				id: 2,
+				name: "Laptops",
+				clientNIC: "222444888",
+				createdAt: new Date("2025-06-24T17:48:19.422Z"),
+				updatedAt: new Date("2025-06-24T17:48:19.422Z"),
+			},
+		]);
+	},
+
 	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete();
+		await queryInterface.bulkDelete("FolderInterests", null, {});
 	},
 };
--- server/seeders/20250602142651-folderInterestsEquipments.js
+++ server/seeders/20250602142651-folderInterestsEquipments.js
@@ -1,25 +1,34 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert("FolderInterestEquipments", [
+			{
+				id: 1,
+				folderInterestId: 1,
+				interestId: 1,
+				createdAt: new Date("2025-06-24T17:44:34.188Z"),
+				updatedAt: new Date("2025-06-24T17:44:34.188Z"),
+			},
+			{
+				id: 2,
+				folderInterestId: 1,
+				interestId: 3,
+				createdAt: new Date("2025-06-24T17:44:35.602Z"),
+				updatedAt: new Date("2025-06-24T17:44:35.602Z"),
+			},
+			{
+				id: 3,
+				folderInterestId: 2,
+				interestId: 7,
+				createdAt: new Date("2025-06-24T17:48:25.239Z"),
+				updatedAt: new Date("2025-06-24T17:48:25.239Z"),
+			},
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete("FolderInterestEquipments", null, {});
+	},
 };
