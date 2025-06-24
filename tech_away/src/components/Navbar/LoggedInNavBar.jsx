1555eac5240ffcecacfa5087e4ded59150392a3c
Leonor Cardoso <leonorjcdc@gmail.com>
2025-06-24 16:07:01 +0100

    notificações ta quase la

--- tech_away/src/components/Navbar/LoggedInNavBar.jsx
+++ tech_away/src/components/Navbar/LoggedInNavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
 	Container,
 	Navbar,
@@ -11,8 +11,8 @@ import { useLocation, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthenticationProviders/AuthProvider";
 import { useCart } from "../../contexts/CartProvider";
+import { useInterests } from "../../contexts/InterestsProvider";
 import { Badge } from "primereact/badge";
-import "../../styles/variables.css";
 
 const navItems = [
 	{ name: "Shop", path: "/store" },
@@ -26,6 +26,7 @@ export default function LoggedInNavBar() {
 
 	const { user, logOut } = useAuth();
 	const { numCartItems, openCart } = useCart();
+	const { numNotifications, openNotifications } = useInterests();
 
 	// Função para logout
 	const handleLogout = () => {
@@ -119,8 +120,33 @@ export default function LoggedInNavBar() {
 										className="pi pi-shopping-cart p-overlay-badge px-2"
 										style={{ color: "var(--dark-grey)", fontSize: "20px" }}
 									>
+										{numCartItems > 0 && (
+											<Badge
+												value={numCartItems}
+												style={{
+													fontSize: "10px",
+													backgroundColor: "var(--white)",
+													color: "var(--dark-grey)",
+												}}
+											/>
+										)}
+									</i>
+								</Button>
+							</Nav.Item>
+							<Nav.Item>
+								<Button
+									className="rounded-pill"
+									style={{
+										backgroundColor: "var(--variant-two)",
+										border: "none",
+									}}
+								>
+									<i
+										className="pi pi-bell p-overlay-badge px-2"
+										style={{ color: "var(--dark-grey)", fontSize: "20px" }}
+									>
 										<Badge
-											value={numCartItems}
+											value={1}
 											style={{
 												fontSize: "10px",
 												backgroundColor: "var(--white)",
